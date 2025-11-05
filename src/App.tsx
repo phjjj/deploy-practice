f02b78d812d293edb3cfe39bd5d5a41e1d667c6e
phjjj <201310871+phjjj2@users.noreply.github.com>
2025-11-06 00:27:56 +0900

    fix import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 
 import Test from './page/_Test'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import One from './page/one'
-import Two from './page/two'
-import Three from './page/three'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Test />,
   },
-{
-  path: '/one',
-  element: <One />,
-},
-{
-  path: '/two',
-  element: <Two />,
-},
-{
-  path: '/three',
-  element: <Three />,
-},
+
 ])
 
 function App() {
